c7973491b74f8aa330dfbfa2708525c43ae52255
12137 <1213765871@qq.com>
2021-01-30 21:31:31 +0800

    数组作为形参有点不了解，还有数组及其元素的地址赋值也不了解

---
+++ week1/src/Task0.java
@@ -0,0 +1,7 @@
+public class Task0 {
+    public static void main(String[] args) {
+        int sum[] = {0,1,0,2,0,3};
+        Task4 kk = new Task4();
+        kk.push0(sum);
+    }
+}
--- week1/src/Task4.java
+++ week1/src/Task4.java
@@ -1,8 +1,20 @@
 public class Task4 {
-    public static void main(String[] args) {
-        Task3 kk = new Task3();
-        kk.nizhuan();
+    public void push0(int arr[]) {
+        int arr1[] = new int[arr.length];
+        int sum = 0;
+        int num = 0;
+        for (int i = 0;num <= arr.length - 1;i++){
+            if (arr[num] == 0){
+                num++;
+            }else{
+                arr1[i] = arr[num];
+                sum++;
+            }
+        }
+        for (int i = sum;i <= arr.length - 1;i++){
+            arr[i] = 0;
+        }
+        arr = arr1;
     }
-
 }
 
