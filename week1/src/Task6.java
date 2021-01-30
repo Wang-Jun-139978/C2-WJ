18fbd111148774de33ff64320f716040573ee256
12137 <1213765871@qq.com>
2021-01-30 21:53:30 +0800

    无

---
+++ week1/src/Task6.java
@@ -0,0 +1,13 @@
+public class Task6 {
+    private String name;
+    private int account;
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAccount(int account){
+        this.account = account;
+    }
+    public int getAccount(){
+        return this.account;
+    }
+}
